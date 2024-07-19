5c0e7bef7fa58133791cf406ffd9fdc082e67a66
BergerPerkins <perkinsberger@gmail.com>
2024-07-19 13:07:39 +0530

    data ingestion added

--- demo.py
+++ demo.py
@@ -1,12 +1,8 @@
-from us_visa.logger import logging
-from us_visa.exception import USvisaException
-import sys
+from us_visa.pipline.training_pipeline import TrainPipeline
 
+obj=TrainPipeline()
+obj.run_pipeline()
 
-#logging.info("This is my log file")
-
-try:
-    a=2/0
-
-except Exception as e:
-    raise USvisaException(e, sys)
+# import os
+# mongo_db_url = os.getenv('MONGODB_URL')
+# print(mongo_db_url)
